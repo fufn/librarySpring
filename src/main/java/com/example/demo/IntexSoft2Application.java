2aded205547e8cab5b8136e2dc889e1de95cc4a1
xfufnx <xfufnx@gmail.com>
2022-12-05 15:33:36 +0600

    project initialized

---
+++ src/main/java/com/example/demo/IntexSoft2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntexSoft2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntexSoft2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/entities/Book.java
@@ -0,0 +1,36 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table (name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String author;
+
+    @Column
+    private String description;
+
+    @Column
+    private Integer year;
+
+    @Column
+    private Boolean isBooked;
+
+}
